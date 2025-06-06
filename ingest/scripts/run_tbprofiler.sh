c4bb22b0c101a71ec3f6f49a09fae26f9c5e5953
Kim Andrews <17375001+kimandrews@users.noreply.github.com>
2025-06-05 20:43:02 -0700

    Use fasterq-dump instead of parallel-fastq-dump fasterq-dump is preferred for running the workflow on AWS, but the Fred Hutch cluster can only use fasterq-dump if fastq's are first downloaded to TMPDIR. This code first looks to see if TMPDIR exists, and if not, it will download fastq's to tb_outdir. Presumably the TMPDIR will not exist if we are running on AWS.

--- ingest/scripts/run_tbprofiler.sh
+++ ingest/scripts/run_tbprofiler.sh
@@ -9,6 +9,16 @@ outdir="$4"
 tb_outdir="$5"
 threads="$6"
 
+# If TMPDIR doesn't exist, use tb_outdir. This enables this script
+# to run on the Fred Hutch cluster, which doesn't allow use of Scratch
+# storage with fasterq-dump, and therefore requires files to be
+# saved on TMPDIR before being moved to tb_outdir
+
+if [[ -z "${TMPDIR:-}" || ! -d "$TMPDIR" ]]; then
+    echo "TMPDIR not set or doesn't exist. Using tb_outdir as TMPDIR." >&2
+    TMPDIR="$tb_outdir"
+fi
+
 s3_path="files/workflows/tb/${output_path}"
 
 if ! command -v aws &> /dev/null; then
@@ -32,9 +42,20 @@ else
     if [[ ! -f "$fastq1" ]]; then
         echo "Downloading fastq files…" >&2
         mkdir -p "${outdir}"  # <-- ADDED SAFEGUARD
-        parallel-fastq-dump --split-files --gzip \
-            --sra-id "${sample}" --threads "${threads}" \
-            --outdir "${outdir}"
+        fasterq-dump "${sample}" -e "${threads}" --temp "${TMPDIR}" --outdir "${TMPDIR}"
+        
+        echo "Compressing FASTQ files…" >&2
+        gzip "${TMPDIR}/${sample}_1.fastq"
+        if [[ -f "${TMPDIR}/${sample}_2.fastq" ]]; then
+            gzip "${TMPDIR}/${sample}_2.fastq"
+        fi
+    
+        echo "Moving compressed FASTQ files to ${outdir}…" >&2
+        mv "${TMPDIR}/${sample}_1.fastq.gz" "${fastq1}"
+        if [[ -f "${TMPDIR}/${sample}_2.fastq.gz" ]]; then
+            mv "${TMPDIR}/${sample}_2.fastq.gz" "${fastq2}"
+        fi
+        
     fi
 
     mkdir -p "${tb_outdir}"
